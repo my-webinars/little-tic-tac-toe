dd22d851bb28b0e89d47feb417b7037b0fb2e052
kos_zivenko <kos.zivenko@gmail.com>
2025-01-08 16:52:52 +0100

    fixed: circular import

--- library/src/tic_tac_toe/logic/validators.py
+++ library/src/tic_tac_toe/logic/validators.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tic_tac_toe.logic.models import Grid
+
 import re
 
-from tic_tac_toe.logic.models import Grid
 
 def validate_grid(grid: Grid) -> None:
     if not re.match(r"^[\sXO]{9}$", grid.cells):
