31ea513e6115ea5364374d72ecdacc8ffd0b16e8
kos_zivenko <kos.zivenko@gmail.com>
2025-01-09 18:29:30 +0100

    added: ConsolePlayer in test script

--- frontend/console/players.py
+++ frontend/console/players.py
@@ -1,6 +1,9 @@
+import re
+
 from tic_tac_toe.game.players import Player
 from tic_tac_toe.logic.exceptions import InvalidMove
 from tic_tac_toe.logic.models import GameState, Move
+
 
 class ConsolePlayer(Player):
     def get_move(self, game_state: GameState) -> Move | None:
@@ -15,3 +18,13 @@ class ConsolePlayer(Player):
                 except InvalidMove:
                     print("That cell is already occupied.")
         return None
+
+
+def grid_to_index(grid: str) -> int:
+    if re.match(r"[abcABC][123]", grid):
+        col, row = grid
+    elif re.match(r"[123][abcABC]", grid):
+        row, col = grid
+    else:
+        raise ValueError("Invalid grid coordinates")
+    return 3 * (int(row) - 1) + (ord(col.upper()) - ord("A"))
