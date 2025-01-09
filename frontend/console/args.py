78c3ef96351ecb213a1244f0af5f0abe33873413
kos_zivenko <kos.zivenko@gmail.com>
2025-01-09 18:56:45 +0100

    added: args.py

--- frontend/console/args.py
+++ frontend/console/args.py
@@ -1,8 +1,42 @@
-from tic_tac_toe.game.players import RandomComputerPlayer
+import argparse
 
+from tic_tac_toe.game.players import Player, RandomComputerPlayer
+from tic_tac_toe.logic.models import Mark
 from .players import ConsolePlayer
 
 PLAYER_CLASSES = {
     "human": ConsolePlayer,
     "random": RandomComputerPlayer,
 }
+
+
+def parse_args() -> tuple[Player, Player, Mark]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-X",
+        dest="player_x",
+        choices=PLAYER_CLASSES.keys(),
+        default="human",
+    )
+    parser.add_argument(
+        "-O",
+        dest="player_o",
+        choices=PLAYER_CLASSES.keys(),
+        default="random",
+    )
+    parser.add_argument(
+        "--starting",
+        dest="starting_mark",
+        choices=Mark,
+        type=Mark,
+        default="X",
+    )
+    args = parser.parse_args()
+
+    player1 = PLAYER_CLASSES[args.player_x](Mark("X"))
+    player2 = PLAYER_CLASSES[args.player_o](Mark("O"))
+
+    if args.starting_mark == "O":
+        player1, player2 = player2, player1
+
+    return player1, player2, args.starting_mark
